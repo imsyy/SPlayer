b76a42081f69a132445c5841c09e9bdf3784f922
aln <w@imgod.me>
2023-09-09 16:47:53 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -178,13 +178,13 @@ onMounted(() => {
   }
 
   // 版权声明
-  const logoText = import.meta.env.VITE_SITE_TITLE;
-  const copyrightNotice = `\n\n版本: ${packageJson.version}\n作者: ${packageJson.author}\n作者主页: ${packageJson.home}\nGitHub: ${packageJson.github}`;
-  console.info(
-    `%c${logoText} %c ${copyrightNotice}`,
-    "color:#f55e55;font-size:26px;font-weight:bold;",
-    "font-size:16px"
-  );
+  // const logoText = import.meta.env.VITE_SITE_TITLE;
+  // const copyrightNotice = `\n\n版本: ${packageJson.version}\n作者: ${packageJson.author}\n作者主页: ${packageJson.home}\nGitHub: ${packageJson.github}`;
+  // console.info(
+  //   `%c${logoText} %c ${copyrightNotice}`,
+  //   "color:#f55e55;font-size:26px;font-weight:bold;",
+  //   "font-size:16px"
+  // );
   console.info(
     "若站点出现异常，可尝试在下方输入 %c$cleanAll()%c 然后按回车来重置",
     "background: #eaeffd;color:#f55e55;padding: 4px 6px;border-radius:8px;",
