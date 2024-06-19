7a24121feb035f2958f07373164003343bfb6d08
xheshan1 <3131654763@qq.com>
2024-06-19 12:08:35 +0800

    修改路由模式为hash模式

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import routes from "@/router/routes";
 
 // 基础配置
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
